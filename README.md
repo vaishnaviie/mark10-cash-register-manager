c6a1247d20b35c1dc1c0654df2fedfccf0e9ac7e
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-09-21 15:20:42 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cash register manager
+
+An app which takes bill amount and cash given as a input and showcase the how much amount needs to return to the customer in tabular form.
+
+# Technologies used:-
+1. Html
+2. CSS
+3. JavaScript
---
+++ script.js
@@ -0,0 +1,47 @@
+var billAmount=document.querySelector("#bill-amount");
+var cashGiven=document.querySelector("#cash-given");
+var buttonCalculate=document.querySelector("#btn-calculte");
+var errorMsg=document.querySelector("#error-msg");
+var displayNotes=document.querySelectorAll(".noOfNotes");
+
+var arr=[2000,500,100,10,5,1];
+
+function helper(amountToBeReturned){
+    for(let i=0;i<arr.length;i++){
+        var amount=Math.trunc(amountToBeReturned/arr[i]); 
+        amountToBeReturned=amountToBeReturned%arr[i];
+ 
+        displayNotes[i].innerText=amount;
+    }
+}
+
+function displayMessage(msg){
+    errorMsg.innerText=msg;
+}
+
+function calculate(){
+    
+    if(Number(billAmount.value)>0 && Number(cashGiven.value)>0){
+        
+        if(Number(cashGiven.value)>Number(billAmount.value)){
+            var amountToBeReturned=(cashGiven.value)-(billAmount.value);
+            helper(amountToBeReturned);
+            displayMessage(`₹ ${amountToBeReturned} needs to return.`);
+        }
+
+        else if(Number(cashGiven.value)===Number(billAmount.value)){
+            displayMessage(`No amount needs to return.`);
+        }
+
+        else{
+            displayMessage("Bill amount should at least be equal to cash given.");
+        }
+    }
+    
+    else{
+        displayMessage("Enter positive value");
+        
+    }   
+}
+
+buttonCalculate.addEventListener("click",calculate);
